7ad728ef40c974db43b25de00769c03f8619bd40
lgitscape <149660812+lgitscape@users.noreply.github.com>
2025-04-25 20:37:08 +0200

    Add files via upload

--- supabase_client.py
+++ supabase_client.py
@@ -1,5 +1,8 @@
-from supabase import create_client
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
+from supabase import create_client
 
 SUPABASE_URL = os.getenv("https://luexdfkqvvhrnxvgpcqs.supabase.co")
 SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1ZXhkZmtxdnZocm54dmdwY3FzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjczODI1NCwiZXhwIjoyMDU4MzE0MjU0fQ.Fwb4zcjUQ2CtMDrmVut4lHy1xGwDdDoqPcTrHcpKQEQ")  # gebruik de service role key
