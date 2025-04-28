7886b2f88a3fcc8f346ad404c82c97517584baf1
lgitscape <149660812+lgitscape@users.noreply.github.com>
2025-04-29 00:34:40 +0200

    Add files via upload

--- tests/test_ann_cache.py
+++ tests/test_ann_cache.py
@@ -19,19 +19,24 @@ class DummyClient:
         self.lon = 4.41
         self.gender_pref = None
         self.therapy_goals = ["stabiliseren"]
+        self.client_traits = ["Volwassenen"]
+        self.expat = False
+        self.lgbtqia = False
+        self.max_km = 25
 
 class DummyTherapist:
     def __init__(self, id):
         self.id = id
         self.topics = ["stress"]
         self.setting = "online"
+        self.client_groups = ["Volwassenen"]
+        self.style = "Warm"
+        self.therapist_goals = ["stabiliseren"]
         self.languages = ["nl"]
         self.timeslots = ["avond"]
         self.fee = 90
         self.contract_with_insurer = True
-        self.style = "Warm"
-        self.therapist_goals = []
-        self.client_groups = []
+        self.gender_pref = None
         self.lat = 52.01
         self.lon = 4.41
 
